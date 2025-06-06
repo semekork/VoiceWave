dbbc8ee65df102f8f0f27a0e99b7da78b5052854
Caleb Dussey <119165434+semekork@users.noreply.github.com>
2025-06-06 17:02:25 +0000

    Update navigationHelpers.js

--- navigation/navigationHelpers.js
+++ navigation/navigationHelpers.js
@@ -1,65 +1,29 @@
-import { CommonActions, StackActions } from '@react-navigation/native';
+let _navigator;
 
-export class NavigationService {
-  static navigator = null;
-
-  static setTopLevelNavigator(navigatorRef) {
-    this.navigator = navigatorRef;
-  }
-
-  static navigate(routeName, params = {}) {
-    if (this.navigator) {
-      this.navigator.navigate(routeName, params);
-    }
-  }
-
-  static push(routeName, params = {}) {
-    if (this.navigator) {
-      this.navigator.dispatch(StackActions.push(routeName, params));
-    }
-  }
-
-  static pop(count = 1) {
-    if (this.navigator) {
-      this.navigator.dispatch(StackActions.pop(count));
-    }
-  }
-
-  static reset(routeName, params = {}) {
-    if (this.navigator) {
-      this.navigator.dispatch(
-        CommonActions.reset({
-          index: 0,
-          routes: [{ name: routeName, params }],
-        })
-      );
-    }
-  }
-
-  static goBack() {
-    if (this.navigator) {
-      this.navigator.goBack();
-    }
-  }
-
-  static getCurrentRoute() {
-    if (this.navigator) {
-      return this.navigator.getCurrentRoute();
-    }
-    return null;
-  }
-
-  static canGoBack() {
-    if (this.navigator) {
-      return this.navigator.canGoBack();
-    }
-    return false;
-  }
-
-  static getState() {
-    if (this.navigator) {
-      return this.navigator.getState();
-    }
-    return null;
-  }
+function setTopLevelNavigator(navigatorRef) {
+  _navigator = navigatorRef;
 }
+
+function navigate(routeName, params) {
+  _navigator.navigate(routeName, params);
+}
+
+function goBack() {
+  _navigator.goBack();
+}
+
+function reset(state) {
+  _navigator.reset(state);
+}
+
+function canGoBack() {
+  return _navigator.canGoBack();
+}
+
+export const NavigationService = {
+  setTopLevelNavigator,
+  navigate,
+  goBack,
+  reset,
+  canGoBack,
+};
